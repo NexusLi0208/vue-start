2550b4f13a2a58d30ce153c98002bc1f643de7ce
Li Peng <327263458@qq.com>
2016-10-31 16:13:17 +0800

    第一次备份

---
+++ static/js/common.js
@@ -0,0 +1,57 @@
+// 初始化代码:
+$(function () {
+    var vm = new Vue({
+        el: '#vm',
+        data: {
+            name: 'Robot',
+            age: 15,
+            show: false,
+            city: 'sh',
+            item: {
+                id: '',
+                province: ''
+            },
+            items: [
+            ]
+        },
+        methods: {
+            register: function () {
+                // 显示JSON格式的Model:
+                alert(JSON.stringify(this.$data));
+                // TODO: AJAX POST...
+            },
+            save: function () {
+                if (this.item.id == "" || this.item.province =="") {
+                    alert('请输入完整案例信息')
+                }
+                else {
+                    this.items.push(this.item);
+                    //将input中的数据重置
+                    this.item = '';
+                    vm.show = false;
+                }
+            },
+            getItem: function () {
+                $.ajax({
+                    type: "GET",
+                    url: 'http://www.bizideal.cn:8082/userManager/province/getlist.do',
+                    dataType: "json",
+                    success: function (data) {
+                        var obj = jQuery.parseJSON(data);
+                        for (var i in obj) {
+                            vm.items.push(obj[i]);
+                        }
+                    }
+                });
+            },
+            removeItem: function () {
+                vm.items = [];
+            },
+            delItem: function (index) {
+                this.items.splice(index, 1)
+            }
+        }
+    });
+    window.vm = vm;
+
+})
